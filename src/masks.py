ca397a8e487248f93de71f69d4c78d19885bde28
darina smirnova <darismi01@yandex.ru>
2024-07-06 11:21:49 +0300

    Создана функция маскировки номера карты get_mask_card_number и счета get_mask_account

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+def get_mask_card_number(card_num: str) -> str:
+    """Маскирует номер карты"""
+    len_num = len(card_num)
+    card_num_mask = card_num.replace(card_num[(len_num - 10) : (len_num - 4)], "** **** ")
+    new_len = len(card_num_mask)
+    num_list = list(card_num_mask)
+    num_list[new_len - 15] = card_num_mask[new_len - 15] + " "
+    card_num_masked = "".join(num_list)
+
+    return card_num_masked
+
+
+def get_mask_account(acc_num: str) -> str:
+    """Маскирует номер счёта"""
+    len_num = len(acc_num)
+    if acc_num is not None:
+        acc_num_masked = acc_num.replace(acc_num[(len_num - 20) : (len_num - 4)], "**")
+    else:
+        acc_num_masked = "Неизвестный отправитель"
+
+    return acc_num_masked
