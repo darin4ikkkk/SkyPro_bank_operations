a571cdef1906c7e6b641a08a365823b11da17d51
darina smirnova <darismi01@yandex.ru>
2024-07-15 16:53:07 +0300

    Добавлен файл README.md Незначительные изменения в оформлении кода

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
-from typing import Optional, Any
+from typing import Optional
+
 
 def filter_by_state(data: list, state_key: Optional[str] = "EXECUTED") -> list:
-    "Отсортировывает только успешные операции"
+    """Отсортировывает только успешные операции"""
     executed = []
     for item in data:
         key = item.get('state')
@@ -11,9 +12,11 @@ def filter_by_state(data: list, state_key: Optional[str] = "EXECUTED") -> list:
 
 
 def sort_key(data: dict) -> str:
+    """Получает дату из словаря по ключу 'date'"""
     return data.get('date')
 
+
 def sort_by_date(data: list, reverse_key: Optional[bool] = True) -> list:
+    """Сортирует операции по дате"""
     sorted_operations = sorted(data, key=sort_key, reverse=reverse_key)
     return sorted_operations
-
