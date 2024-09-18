1b243ffb1cff1e2bf7031bbeeed7f29a24b80dc8
darina smirnova <darismi01@yandex.ru>
2024-09-18 21:46:22 +0300

    написан модуль decorators и тесты к нему

--- src/decorators.py
+++ src/decorators.py
@@ -1,5 +1,4 @@
 from functools import wraps
-from datetime import time
 
 
 def log(filename=None):
@@ -9,22 +8,18 @@ def log(filename=None):
         def wrapper(*args, **kwargs):
             result = None
             try:
-                time_1 = time()
                 result = func(*args, **kwargs)
-                time_2 = time()
                 if filename:
                     with open(filename, 'a', encoding='utf-8') as file:
-                        file.write(f"{func.__name__} OK / time_start = {time_1}, time_end = {time_2}")
+                        file.write(f"{func.__name__} OK")
                 else:
-                    print(f"{func.__name__} OK / time_start = {time_1}, time_end = {time_2}")
+                    print(f"{func.__name__} OK")
             except Exception as e:
                 if filename:
-                    time_2 = time()
                     with open(filename, 'a', encoding='utf-8') as file:
-                        file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs} / time_start = {time_1}, time_end = {time_2}")
+                        file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
                 else:
-                    time_2 = time()
-                    print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs} / time_start = {time_1}, time_end = {time_2}")
+                    print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
             return result
         return wrapper
     return my_decorator
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -8,7 +8,7 @@ def my_function_console(x: int, y: int):
     """Функция для проверки работы декоратора log (вывод в консоль)"""
     return x/y
 
-filename = os.path.abspath("/Users/darinasmirnova/PycharmProjects/SkyPro91/logs/my_log.txt")
+filename = "/Users/darinasmirnova/PycharmProjects/SkyPro91/logs/my_log.txt"
 @log(filename)
 def my_function_file(x: int, y: int):
     """Функция для проверки работы декоратора log (запись в файл)"""
@@ -18,26 +18,26 @@ def my_function_file(x: int, y: int):
 def test_dec_log_console_ok(capsys):
     my_function_console(2, 1)
     captured = capsys.readouterr()
-    assert captured.out == 'my_function_console OK / time_start = 00:00:00, time_end = 00:00:00\n'
+    assert captured.out == 'my_function_console OK\n'
 
 def test_dec_log_file_ok():
     my_function_file(2, 1)
     with open(filename, 'r', encoding='utf-8') as file:
         logs = file.read()
-    assert 'my_function_file OK / time_start = 00:00:00, time_end = 00:00:00' in logs
+    assert 'my_function_file OK' in logs
 
 def test_dec_log_console_error(capsys):
     my_function_console(2, 0)
     captured = capsys.readouterr()
-    assert captured.out == 'my_function_console error: division by zero. Inputs: (2, 0), {} / time_start = 00:00:00, time_end = 00:00:00\n'
+    assert captured.out == 'my_function_console error: division by zero. Inputs: (2, 0), {}\n'
 
     my_function_console(2, '1')
     captured = capsys.readouterr()
-    assert captured.out == "my_function_console error: unsupported operand type(s) for /: 'int' and 'str'. Inputs: (2, '1'), {} / time_start = 00:00:00, time_end = 00:00:00\n"
+    assert captured.out == "my_function_console error: unsupported operand type(s) for /: 'int' and 'str'. Inputs: (2, '1'), {}\n"
 
 def test_dec_log_file_error():
     my_function_file(2, 0)
     with open(filename, 'r', encoding='utf-8') as file:
         logs = file.read()
-    assert 'my_function_file error: division by zero. Inputs: (2, 0), {} / time_start = 00:00:00, time_end = 00:00:00' in logs
+    assert 'my_function_file error: division by zero. Inputs: (2, 0), {}' in logs
 
